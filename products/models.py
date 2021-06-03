a47ffed98dbef4d567acddf7c65543dc40a16455
h00gin <h00gin@yandex/ru>
2021-06-03 17:45:10 +0300

    create superuser

--- products/models.py
+++ products/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class ProductCategory(models.Model):
     name = models.CharField(max_length=64, unique=True)
     description = models.TextField(blank=True)
+    
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
@@ -13,4 +16,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
