e0d127d6d49de57f68096883f17f65e2d361fcb0
h00gin <h00gin@yandex/ru>
2021-08-24 00:43:45 +0300

    [fix] changed context_processors.py

--- products/context_processors.py
+++ products/context_processors.py
@@ -4,7 +4,7 @@ from baskets.models import Basket
 def basket(request):
     basket_list = []
     if request.user.is_authenticated:
-        basket_list = Basket.objects.filter(user=request.user).select_related('category')[:3]
+        basket_list = Basket.objects.filter(user=request.user)
     # basket_list = request.user.basket.select_related()
 
     return {
