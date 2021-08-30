f16fa64311749a858bd6aa1f0b5188f33bd93fae
h00gin <h00gin@yandex/ru>
2021-08-30 23:10:33 +0300

    added test_product_list and test_categories_list tests.py

--- products/tests.py
+++ products/tests.py
@@ -1,13 +1,39 @@
 from django.test import TestCase, Client
 
+from products.models import ProductCategory, Product
+
 
 class MainAppSmokeTest(TestCase):
+    status_code_success = 200
     
     def setUp(self):
+        category = ProductCategory.objects.create(
+            name='cat1'
+        )
+        for i in range(10):
+            Product.objects.create(
+                category=category,
+                name=f'prod{i}',
+            )
         self.client = Client()
 
     def test_main_app_urls(self):
         response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, self.status_code_success)
+
+        response = self.client.get('/products/')
+        self.assertEqual(response.status_code, self.status_code_success)
+
+    def test_products_list(self):
+        for product_item in Product.objects.all():
+            response = self.client.get(f'/products/{product_item.pk}')
+            self.assertEqual(response.status_code, self.status_code_success)
+
+    def test_categories_list(self):
+        for category_item in ProductCategory .objects.all():
+            response = self.client.get(f'/products/{category_item.pk}')
+            self.assertEqual(response.status_code, self.status_code_success)
+
+
 
 
