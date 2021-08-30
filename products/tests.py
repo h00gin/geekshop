73bcbb448beb28174f4fa5836a31218dd5535799
h00gin <h00gin@yandex/ru>
2021-08-30 22:22:26 +0300

    created tests.py

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+
+class MainAppSmokeTest(TestCase):
+    
+    def setUp(self):
+        self.client = Client()
+
+    def test_main_app_urls(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+
