d381a57fac503f978e40bd289d617ed5c8b6ac00
h00gin <h00gin@yandex/ru>
2021-08-31 00:05:26 +0300

    [fix]added test_product_list and test_categories_list tests.py

--- products/tests.py
+++ products/tests.py
@@ -24,10 +24,10 @@ class MainAppSmokeTest(TestCase):
         response = self.client.get('/products/')
         self.assertEqual(response.status_code, self.status_code_success)
 
-    # def test_products_list(self):
-    #     for product_item in Product.objects.all():
-    #         response = self.client.get(f'/products/{product_item.pk}')
-    #         self.assertEqual(response.status_code, self.status_code_success)
+    def test_products_list(self):
+        for product_item in Product.objects.all():
+            response = self.client.get(f'/products/{product_item.pk}')
+            self.assertEqual(response.status_code, self.status_code_success)
 
     def test_categories_list(self):
         for category_item in ProductCategory .objects.all():
