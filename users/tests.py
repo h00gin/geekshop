172509da7b0bc65368aac833a143f783a8582a0b
h00gin <h00gin@yandex/ru>
2021-08-31 23:03:41 +0300

    created tests.py in users app

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+from users.models import User
+
+
+class UserAuthTestCase(TestCase):
+    status_code_success = 200
+    status_code_redirect = 302
+    status_code_forbidden = 403
+
+    def setUp(self):
+        self.client = Client()
+
+        self.user = User.objects.create_user(
+            username='django2',
+            email='django2@ya.ru',
+            password='geekbrains',
+        )
+
+    def test_login_user(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, self.status_code_success)
+        self.assertTrue(response.context['user'].is_anonymous)
+        self.assertNotContains(response, 'Пользователь', status_code=self.status_code_success)
+
