769d8124b90f553c7139e3c950588fd0e03c4627
h00gin <h00gin@yandex/ru>
2021-08-31 21:53:24 +0300

    [fix] tests.py

--- products/tests.py
+++ products/tests.py
@@ -4,7 +4,7 @@ from products.models import ProductCategory, Product
 
 
 class MainAppSmokeTest(TestCase):
-    status_code_success = 305
+    status_code_success = 200
     
     def setUp(self):
         category = ProductCategory.objects.create(
@@ -19,20 +19,20 @@ class MainAppSmokeTest(TestCase):
         self.client = Client()
 
     def test_main_app_urls(self):
-        response = self.client.get('/')
+        response = self.client.get('')
         self.assertEqual(response.status_code, self.status_code_success)
 
-        response = self.client.get('/products/')
+        response = self.client.get('products/')
         self.assertEqual(response.status_code, self.status_code_success)
 
     def test_products_list(self):
         for product_item in Product.objects.all():
-            response = self.client.get(f'/products/{product_item.pk}')
+            response = self.client.get(f'products/{product_item.pk}')
             self.assertEqual(response.status_code, self.status_code_success)
 
     def test_categories_list(self):
         for category_item in ProductCategory .objects.all():
-            response = self.client.get(f'/products/{category_item.pk}')
+            response = self.client.get(f'products/{category_item.pk}')
             self.assertEqual(response.status_code, self.status_code_success)
 
 
