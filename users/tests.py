9f55e01edea10b2e59fb665d86219ba26e56c360
h00gin <h00gin@yandex/ru>
2021-09-01 23:34:57 +0300

    [fix]added test_register_user in tests.py in users app

--- users/tests.py
+++ users/tests.py
@@ -56,7 +56,7 @@ class UserAuthTestCase(TestCase):
 
         new_user = User.objects.get(username=new_user_data['username'])
 
-        activation_url = f'{settings.DOMAIN_NAME}/users/verify/{new_user_data["email"]}/{new_user.activation_key}'
+        activation_url = f"{settings.DOMAIN_NAME}/users/verify/{new_user_data['email']}/{new_user.activation_key}"
         response = self.client.get(activation_url)
         self.assertEqual(response.status_code, self.status_code_success_redirect)
 
