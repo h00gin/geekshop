a50e7eca7cc5c5513395694cb938cd08431fdcac
h00gin <h00gin@yandex/ru>
2021-09-01 22:25:54 +0300

    added test_register_user in tests.py in users app

--- users/tests.py
+++ users/tests.py
@@ -1,11 +1,13 @@
 from django.test import TestCase, Client
 
+from geekshop import settings
 from users.models import User
 
 
 class UserAuthTestCase(TestCase):
     status_code_success = 200
     status_code_redirect = 302
+    status_code_success_redirect = 301
     status_code_forbidden = 403
     username = 'django2'
     user_password = 'geekbrains'
@@ -37,4 +39,27 @@ class UserAuthTestCase(TestCase):
         self.assertEqual(response.status_code, self.status_code_success)
         self.assertFalse(response.context['user'].is_anonymous)
         self.assertContains(response, f'{self.username}', status_code=self.status_code_success)
+
+    def test_register_user(self):
+        new_user_data = {
+            'username': 'django3',
+            'email': 'django3@yandex.ru',
+            'password1': self.user_password,
+            'password2': self.user_password
+        }
+
+        response = self.client.post('/users/register/', data=new_user_data)
+        self.assertEqual(response.status_code, self.status_code_redirect)
+
+        new_user = User.objects.get(username=new_user_data['username'])
+
+        activation_url = f'{settings.DOMAIN_NAME}/users/verify/{new_user_data["email"]}/{nem_user.activation_key}'
+        response = self.client.get(activation_url)
+        self.assertEqual(response.status_code, self.status_code_success_redirect)
+
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, self.status_code_success)
+        self.assertContains(response, f'{self.username}', status_code=self.status_code_success)
+
+
 
